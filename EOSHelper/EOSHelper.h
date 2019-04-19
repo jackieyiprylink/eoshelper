2f1fe6cb24ad9638b8a9a59f714e830e4b885801
Kiyo <maoxin.cao@rylink.com>
2019-04-19 23:40:58 +0800

    init

---
+++ EOSHelper/EOSHelper.h
@@ -0,0 +1,27 @@
+//
+//  EOSHelper.h
+//  EOSHelper
+//
+//  Created by aa on 2019/4/3.
+//  Copyright © 2019 aa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for EOSHelper.
+FOUNDATION_EXPORT double EOSHelperVersionNumber;
+
+//! Project version string for EOSHelper.
+FOUNDATION_EXPORT const unsigned char EOSHelperVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EOSHelper/PublicHeader.h>
+
+
+#import <EOSHelper/sha2.h>
+#import <EOSHelper/uECC.h>
+#import <EOSHelper/libbase58.h>
+#import <EOSHelper/rmd160.h>
+#import <EOSHelper/NSObject+Extension.h>
+#import <EOSHelper/EOS_Key_Encode.h>
+
+
